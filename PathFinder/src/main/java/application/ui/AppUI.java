f95039bc64ee12a03da9c950a715aa1770415814
guotin <eero.kauhanen@helsinki.fi>
2019-11-14 22:12:09 +0200

    käyttöliittymän aloitusta

--- PathFinder/src/main/java/application/ui/AppUI.java
+++ PathFinder/src/main/java/application/ui/AppUI.java
@@ -1,7 +1,106 @@
-
 package application.ui;
 
+import application.domain.Filereader;
+import application.domain.Routefinder;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class AppUI {
+public class AppUI extends Application {
+
+    private Filereader reader;
+    private Routefinder route;
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private char[][] currentMap;
+    private boolean map1;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        reader = new Filereader();
+        route = new Routefinder();
+        loadMap("map1.map");
+        map1 = true;
+        initCanvas();
+        drawMap();
+        GridPane buttonGrid = new GridPane();
+        Button button1 = new Button("Solve");
+        Button button2 = new Button("Change Map");
+        buttonGrid.add(button1, 0, 0);
+        buttonGrid.add(button2, 1, 0);
+        ScrollPane scrollPane = new ScrollPane(canvas);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(scrollPane);
+        borderPane.setBottom(buttonGrid);
+
+        button1.setOnAction((event) -> {
+            findRoute();
+            drawMap();
+        });
+        
+        button2.setOnAction((event) -> {
+            if (map1) {
+                loadMap("map2.map");
+            } else {
+               loadMap("map1.map"); 
+            }
+            drawMap();
+            map1 = !map1;
+            
+        });
+
+        borderPane.setPrefSize(1024, 720);
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
+    private void initCanvas() {
+        canvas = new Canvas(1024, 1024);
+        gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, 1024, 1024);
+    }
+
+    private void drawMap() {
+        gc.scale(2, 2);
+        for (int i = 0; i < currentMap.length; i++) {
+            for (int j = 0; j < currentMap[0].length; j++) {
+                if (currentMap[i][j] == '.') {
+                    gc.setFill(Color.WHITE);
+                } else if (currentMap[i][j] == 'G') {
+                    gc.setFill(Color.RED);
+                } else if (currentMap[i][j] == 'S') {
+                    gc.setFill(Color.BLUE);
+                } else if (currentMap[i][j] == 'p') {
+                    gc.setFill(Color.GREEN);
+                } else {
+                    gc.setFill(Color.BLACK);
+                }
+                gc.fillRect(i, j, 1, 1);
+            }
+        }
+        gc.scale(0.5, 0.5);
+
+    }
+
+    private void findRoute() {
+        route.setMap(currentMap);
+        route.findStartAndGoal();
+        route.findRouteAstar();
+        currentMap = route.copyArray(route.getAstarMap());
+    }
+
+    private void loadMap(String file) {
+        char[][] map = reader.readFile(file, 512, 512);
+        currentMap = map;
+    }
+
 }
--- PathFinder/src/main/java/application/ui/Main.java
+++ PathFinder/src/main/java/application/ui/Main.java
@@ -2,34 +2,13 @@ package application.ui;
 
 import application.domain.Filereader;
 import application.domain.Routefinder;
+import static javafx.application.Application.launch;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        Filereader reader = new Filereader();
-        char[][] map = reader.readFile("testmap1.map", 2, 6);
-        System.out.println("Printing map");
-        for (int i = 0; i < map.length; i++) {
-            System.out.println("");
-            for (int j = 0; j < map[0].length; j++) {
-                System.out.print(map[i][j]);
-            }
-        }
-        Routefinder route = new Routefinder();
-        route.setMap(map);
-        route.findStartAndGoal();
-        route.findRouteDijkstra();
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Dijkstra path");
-        char[][] dijkstra = route.getDijkstraMap();
-        for (int i = 0; i < map.length; i++) {
-            System.out.println("");
-            for (int j = 0; j < map[0].length; j++) {
-                System.out.print(dijkstra[i][j]);
-            }
-        }               
+        launch(AppUI.class);             
     }
 
 }
